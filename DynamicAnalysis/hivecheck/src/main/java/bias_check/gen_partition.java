2fe0bb0ee916bc39fa0bb05ba4820575e2871c41
ChrisCN97 <874147338@qq.com>
2020-10-30 13:33:52 +0800

    更新了Readme和架构文档
    还更新了数据生成方法gen_search；
    下一步可能会编写一个网格搜索方法grid_search来探究apReduce数量设置不合理的具体表现

--- DynamicAnalysis/hivecheck/src/main/java/bias_check/gen_partition.java
+++ DynamicAnalysis/hivecheck/src/main/java/bias_check/gen_partition.java
@@ -2,13 +2,20 @@ package bias_check;
 
 import java.io.*;
 import java.util.Random;
-
+// sudo docker cp /mnt/sda/cn/hiveAntipattern/data/mapReduceNumberTest_50kAverage.txt myhive:/home
+// sudo docker exec -it myhive /bin/bash
+// /usr/hive/apache-hive-2.3.4-bin/bin/hive
+// create table mrtest_50kaverage (name string, age int, city string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
+// load data local inpath '/home/mapReduceNumberTest_50kAverage.txt' into table mrtest_50kaverage;
+// set mapred.reduce.tasks=10;
+// set hive.auto.convert.join=false;
+// SELECT n.name, a.age FROM mapReduceNumberTest_xk n JOIN mapReduceNumberTest_xk a ON n.loc=a.loc;
 public class gen_partition {
     public static String alphab="abcdefghijklmnopqrstuvwxyz";
     public static void main(String[] args) {
-        String loc="shanghai,shanghai";
-        int lines=6000000;
-        String path = "E://paritition_shanghai.txt";
+        String[] locs={"A","B","C","D","E"};
+        int[] lines={10000,10000,10000,10000,10000};
+        String path = "E:/test/data/mapReduceNumberTest_50kAverage.txt";
         File file = new File(path);
         try {
             if (file.exists()) {
@@ -19,10 +26,12 @@ public class gen_partition {
             OutputStreamWriter osw=new OutputStreamWriter(fos, "UTF-8");
             BufferedWriter  bw=new BufferedWriter(osw);
             Random rand=new Random();
-            for(int i=0;i<lines;i++){
-                String name=""+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25));
-                int age=rand.nextInt(55)+20;
-                bw.write(name+","+ age+"," +loc+'\n');
+            for(int n=0; n<5; n++){
+                for(int i=0;i<lines[n];i++){
+                    String name=""+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25));
+                    int age=rand.nextInt(55)+20;
+                    bw.write(name+","+ age+"," +locs[n]+'\n');
+                }
             }
             bw.close();
             osw.close();

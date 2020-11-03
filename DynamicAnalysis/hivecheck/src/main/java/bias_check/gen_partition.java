b4680cb2a6798f34049360d44ecf8914cba50ba1
ChrisCN97 <874147338@qq.com>
2020-11-03 19:45:03 +0800

    update  grid_search
    For some experiment

--- DynamicAnalysis/hivecheck/src/main/java/bias_check/gen_partition.java
+++ DynamicAnalysis/hivecheck/src/main/java/bias_check/gen_partition.java
@@ -6,25 +6,18 @@ import java.util.Random;
 public class gen_partition {
     public static String alphab="abcdefghijklmnopqrstuvwxyz";
     public static void main(String[] args) {
-        String[] locs={"A","B","C","D","E"};
-        int[] lines={10000,10000,10000,10000,10000};
-        String path = "E:/test/data/mapReduceNumberTest_50kAverage.txt";
+        int lines=5000;
+        String path = "E:/test/data/mrtest_300c.txt";
         File file = new File(path);
         try {
-            if (file.exists()) {
-                //判断文件是否存在，如果不存在就新建一个txt
-                file.createNewFile();
-            }
             FileOutputStream fos=new FileOutputStream(file);
             OutputStreamWriter osw=new OutputStreamWriter(fos, "UTF-8");
             BufferedWriter  bw=new BufferedWriter(osw);
             Random rand=new Random();
-            for(int n=0; n<5; n++){
-                for(int i=0;i<lines[n];i++){
-                    String name=""+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25))+alphab.charAt(rand.nextInt(25));
-                    int age=rand.nextInt(55)+20;
-                    bw.write(name+","+ age+"," +locs[n]+'\n');
-                }
+            for(int i=0;i<lines;i++){
+                String name=""+alphab.charAt(rand.nextInt(26))+alphab.charAt(rand.nextInt(26))+alphab.charAt(rand.nextInt(26))+alphab.charAt(rand.nextInt(26));
+                int age=rand.nextInt(55)+20;
+                bw.write(name+","+ age+"," +rand.nextInt(300)+'\n');
             }
             bw.close();
             osw.close();
--- DynamicAnalysis/hivecheck/src/main/java/bias_check/grid_search.java
+++ DynamicAnalysis/hivecheck/src/main/java/bias_check/grid_search.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 // 自动执行实验
 public class grid_search {
     // 执行SQL语句
-    private static void execSQL(int reduceNum, int iter, FileWriter f){
+    private static void execSQL(String[] conf, String sql, FileWriter f, String comment){
         try {
             Class.forName("org.apache.hive.jdbc.HiveDriver");
             FileInputStream in = new FileInputStream("src/main/java/bias_check/application.properties");
@@ -17,23 +17,21 @@ public class grid_search {
             props.load(in);
             String url = props.getProperty("url");
             Connection connection = DriverManager.getConnection(url,props.getProperty("username"),props.getProperty("password"));
-            String sql1="set mapred.reduce.tasks="+reduceNum;
-            String sql2="set hive.auto.convert.join=false";
-            String sql3="SELECT n.name, a.age FROM mrtest_50kaverage n JOIN mrtest_50kaverage a ON n.city=a.city";
             Statement ps=connection.createStatement();
-            ps.execute(sql1);
-            ps.execute(sql2);
-            System.out.print("Reduce Num:"+reduceNum+" ");
+            for(String c : conf){
+                ps.execute(c);
+            }
             long startTime=System.currentTimeMillis();
-            for(int i=0; i<iter; i++){
-                ps.execute(sql3);
+            System.out.print(comment+" ");
+            for(int i=0; i<3; i++){
+                ps.execute(sql);
                 System.out.print("*");
             }
-            long costTime=(System.currentTimeMillis()-startTime)/iter;
+            long costTime=(System.currentTimeMillis()-startTime)/3;
             SimpleDateFormat formatter = new SimpleDateFormat("mm:ss");
             String ms = formatter.format(costTime);
             System.out.println(" Time:"+ms);
-            f.write(""+reduceNum+" "+costTime+" "+ms+"\n");
+            f.write(comment+" "+costTime+" "+ms+"\n");
             ps.close();
             connection.close();
         } catch (Exception e) {
@@ -44,15 +42,34 @@ public class grid_search {
     // 主程序
     public static void main(String[] args){
         try{
-            FileWriter f = new FileWriter("gridSearch.txt");
-            for(int i=1; i<15; i++){
-                execSQL(i, 3, f);
+            FileWriter f = new FileWriter("data/selfJoinOrMapjoin.txt");
+            String[] joinConf = {"set mapred.reduce.tasks=5", "set hive.auto.convert.join=false"};
+            String[] mapJoinConf = {"set hive.auto.convert.join=true"};
+            String[] fileList = {"10", "50", "500", "5k", "50k"};
+            for(int i=0; i<5; i++){
+                for(int j=i; j<5; j++){
+                    String a = "mrtest_" + fileList[i];
+                    String b = "mrtest_" + fileList[j];
+                    String sql = "SELECT a.name, b.age FROM "+a+" a JOIN "+b+" b ON a.city=b.city";
+                    execSQL(joinConf, sql, f, a+" join "+b);
+                    execSQL(mapJoinConf, sql, f, a+" mapJoin "+b);
+                }
             }
-            execSQL(20, 3, f);
-            execSQL(40, 3, f);
-            execSQL(80, 3, f);
-            execSQL(120, 3, f);
             f.close();
+            FileWriter f2 = new FileWriter("data/reduceNum.txt");
+            int[] numList = {1, 3, 5, 10, 30, 50, 100, 300};
+            for(int i=0; i<8; i++){
+                int cityNum = numList[i];
+                String tableName = "mrtest_"+cityNum+"c";
+                for(int j=0; j<8; j++){
+                    int reduceNum = numList[j];
+                    String[] tempConf = {"set mapred.reduce.tasks="+reduceNum, "set hive.auto.convert.join=false"};
+                    String sql = "SELECT a.name, b.age FROM "+tableName+" a JOIN "+tableName+" b ON a.city=b.city";
+                    execSQL(tempConf, sql, f2, "city"+cityNum+" reduce "+reduceNum);
+                }
+                f2.write("\n");
+            }
+            f2.close();
         } catch (IOException e) {
             e.printStackTrace();
         }

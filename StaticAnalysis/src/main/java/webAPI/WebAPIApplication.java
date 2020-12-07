16c728dad3d1c9f330e75b4e4ad8a818dfd39485
yssjtu <635664803@qq.com>
2020-12-07 19:51:42 +0800

    add dynamic web api

---
+++ StaticAnalysis/src/main/java/webAPI/WebAPIApplication.java
@@ -0,0 +1,11 @@
+package webAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class WebAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAPIApplication.class, args);
+    }
+
+}
---
+++ StaticAnalysis/src/main/java/webAPI/staticCheckController.java
@@ -0,0 +1,18 @@
+package webAPI;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import myApplication.MergedTest;
+
+
+@RestController
+public class staticCheckController {
+    @RequestMapping(value="/astCheck",method = RequestMethod.GET)
+    public String astCheck(@RequestParam(name="hiveql")String hiveql){
+        System.out.println(hiveql);
+        MergedTest.astCheck(hiveql);
+        return "123";
+    }
+}

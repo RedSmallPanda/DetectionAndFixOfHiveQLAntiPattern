1c1c64f51912183afc9001b392e47bb5b5671421
DianaRenYao <857644432@qq.com>
2020-12-14 00:56:24 +0800

    修改查看设置文件

--- UploadAndDetect/UploadAndDetect/src/components/SetConfig.vue
+++ UploadAndDetect/UploadAndDetect/src/components/SetConfig.vue
@@ -12,7 +12,7 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button type="primary" @click="onSubmit">Commit </el-button>
       </el-form-item>
     </el-form>
   </div>
--- UploadAndDetect/UploadAndDetect/src/components/View.vue
+++ UploadAndDetect/UploadAndDetect/src/components/View.vue
@@ -1,5 +1,40 @@
 <template>
-    <div>
-        this is view
-    </div>
+  <div>
+    <h1>View Configurations</h1>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="设置1">
+        <el-input v-model="formInline.user" placeholder="审批人" :disabled="true"></el-input>
+      </el-form-item>
+      <el-form-item label="设置2">
+        <el-select v-model="formInline.region" placeholder="活动区域" :disabled="true">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Modify</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formInline: {
+        user: "test user",
+        region: "test region",
+      }
+    }
+  },
+ methods: {
+    onSubmit() {
+      console.log("go to modify!");
+        this.$router.push({
+            path: `/set`
+          })
+    }
+  }
+}
+</script>

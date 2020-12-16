9883d07e261b93ff2fb0a0f437a1d430f9cd9c2a
yssjtu <635664803@qq.com>
2020-12-16 14:06:57 +0800

    static check 错误处理

--- StaticAnalysis/src/main/java/webAPI/staticCheckController.java
+++ StaticAnalysis/src/main/java/webAPI/staticCheckController.java
@@ -13,10 +13,13 @@ public class staticCheckController {
     public String astCheck(@RequestParam(name="hiveql")String hiveql){
         System.out.println(hiveql);
         ReturnMessageEntity returnMessageEntity=StaticCheckImp.staticCheckRun(hiveql);
-        String messageJson= JSON.toJSONString(returnMessageEntity);
-        System.out.println(messageJson);
+        if(returnMessageEntity!=null) {
+            String messageJson = JSON.toJSONString(returnMessageEntity);
+            System.out.println(messageJson);
+            return messageJson;
+        }
 //        MergedTest.astCheck(hiveql);
-        return messageJson;
+        return "hiveql格式错误";
     }
     @RequestMapping(value="/configCheck",method = RequestMethod.GET)
     public String configCheck(){

1a2568789a92149d1ca71ad1449d6909361a93d2
DianaRenYao <857644432@qq.com>
2020-12-21 21:58:24 +0800

    pipeline api1 and api2

--- UploadAndDetect/UploadAndDetect/src/components/Detect.vue
+++ UploadAndDetect/UploadAndDetect/src/components/Detect.vue
@@ -26,7 +26,21 @@
               element-loading-spinner="el-icon-loading"
               element-loading-background="rgba(0, 0, 0, 0.8)"
             >
-            {{ fixedHiveql }}
+            <br>
+            HiveQL检测结果：{{ fixedHiveql }}
+            <br>
+            </div>
+            <div
+              v-if="isGetJoinResult"
+              v-loading="joinLoading"
+              element-loading-text="正在检查数据倾斜"
+              element-loading-spinner="el-icon-loading"
+              element-loading-background="rgba(0, 0, 0, 0.8)"
+              style="margin-top:10px;margin-bottom:10px"
+            >
+            <br>
+            数据倾斜结果：{{ dataImbalancedSuggest }}
+            <br>
             </div>
           </el-card>
         </el-col>
@@ -45,8 +59,22 @@
             >
               <el-table-column prop="id" label="序号" width="80">
               </el-table-column>
-              <el-table-column prop="suggestion" label="修复建议" width="720">
+              <el-table-column prop="suggestion" label="修复建议" >
               </el-table-column>
+            </el-table>
+            <el-table
+              v-if="isGetJoinResult"
+              v-loading="joinLoading"
+              element-loading-text="正在推荐Reduce数量"
+              element-loading-spinner="el-icon-loading"
+              element-loading-background="rgba(0, 0, 0, 0.8)"
+              :data="recommendReduceNum"
+              style="width: 100%"
+            >
+              <el-table-column prop="id" label="序号" width="80">
+              </el-table-column>
+              <el-table-column prop="recommend" label="推荐配置" >
+              </el-table-column>            
             </el-table>
           </el-card>
         </el-col>
@@ -65,6 +93,8 @@ export default {
     return {
       hiveQL: "",
       fixedHiveql: "",
+      dataImbalancedSuggest: "",
+      recommendReduceNum:[],
       fixSuggestions:[
         // {
         //   id: "-1",
@@ -73,18 +103,29 @@ export default {
        ],
       isGetFixResult: false,
       disableHiveQL: false,
+      isGetJoinResult: false,
+      fixLoading: true,
+      joinLoading: true,
       api1url: this.common.api1url,
       api2url: this.common.api2url,
       t1_name: " ",
       t1_key: " ",
       t2_name: " ",
       t2_key: " ",
-      fixLoading: true,
     };
   },
   methods: {
     detect() {
-      var _this = this; //指定this
+      var _this = this; //savve this
+      // clear all history detection
+      _this.isGetFixResult = false;
+      _this.isGetJoinResult = false;
+      _this.fixLoading = true;
+      _this.joinLoading = true;
+      _this.fixedHiveql = "";
+      _this.fixSuggestions = [];
+      _this.dataImbalancedSuggest = "";
+      _this.recommendReduceNum = [];
       _this
         .$axios({
           //创建接口
@@ -105,6 +146,7 @@ export default {
             _this.fixSuggestions.push({"id":i+1,"suggestion":response.data.fixedSuggestions[i]});
           }
           console.log(response.data.fixedHiveql);
+          console.log(response.data.joinParams);
           if (response.data.joinParams){
             _this.t1_name = response.data.joinParams[0];
             _this.t1_key = response.data.joinParams[1];
@@ -116,6 +158,7 @@ export default {
     },
     join_detect(){
       var _this = this;
+      _this.isGetJoinResult = true;
       _this
       .$axios({
         // create api
@@ -127,8 +170,11 @@ export default {
           t2_name:_this.t2_name,
           t2_key:_this.t2_key
         }
-      }).then(function(response){
+      }).then(function(response){    
+        _this.joinLoading = false;
         console.log(response.data); //打印请求的数据
+        _this.dataImbalancedSuggest = response.data.dataImbalancedSuggest;
+        _this.recommendReduceNum = [{"id":1,"recommend":response.data.recommendReduceNum}];
       });
     }
   },

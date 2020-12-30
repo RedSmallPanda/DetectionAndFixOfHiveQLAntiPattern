21d0e31fb387f6c32af34b3711a969eeff49153b
DianaRenYao <857644432@qq.com>
2020-12-30 16:58:21 +0800

    update api doc

--- UploadAndDetect/UploadAndDetect/src/components/SetConfig.vue
+++ UploadAndDetect/UploadAndDetect/src/components/SetConfig.vue
@@ -83,7 +83,7 @@ export default {
           },
         })
         .then(function (response) {
-          console.log(response);
+          console.log(response.data);
           if (response.status == 200) {
             console.log("save successfully");
             _this.$message({
